8aa7e772dd132d76379a3c9c11c7941f26d5a5cf
Felix-alt-cell <rkomilov03@gmail.com>
2026-04-22 12:36:58 +0900

    TaskB: counting digits

--- train.js
+++ train.js
@@ -1,27 +1,66 @@
 /*
-A-TASK (NodeJS)
+Shunday function tuzing, u 1ta string parametrga ega bolsin, 
+hamda osha stringda qatnashgan raqamlarni sonini 
+bizga return qilsin.
 
-Shunday 2 parametrli function tuzing, 
-hamda birinchi parametrdagi letterni ikkinchi parametrdagi
-sozdan qatnashga sonini return qilishi kerak boladi.
-MASALAN countLetter("e", "engineer") 3ni return qiladi.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 */
 
 //Solution
 
-function countLetter(letter, text) {
-    letter = letter.toLowerCase();
-    text = text.toLowerCase();
-
+function countDigits(text) {
     let count = 0;
 
-    for (let i = 0; i < text.length; i++) {
-        if (text[i] === letter) {
+    for (let x = 0; x < text.length; x++) {
+        if (text[x] >= '0' && text[x] <= '9') {
             count++;
         }
     }
 
     return count;
 }
+console.log(countDigits("y0u4c0d1sp9ssw0r6d"))
 
-console.log(countLetter("a", "United States of America"));
+
+
+
+
+
+
+
+
+
+
+
+// /*
+// A-TASK (NodeJS)
+
+// Shunday 2 parametrli function tuzing, 
+// hamda birinchi parametrdagi letterni ikkinchi parametrdagi
+// sozdan qatnashga sonini return qilishi kerak boladi.
+// MASALAN countLetter("e", "engineer") 3ni return qiladi.
+// */
+
+// //Solution
+
+// function countLetter(letter, text) {
+//     letter = letter.toLowerCase();
+//     text = text.toLowerCase();
+
+//     let count = 0;
+
+//     for (let i = 0; i < text.length; i++) {
+//         if (text[i] === letter) {
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
+
+// console.log(countLetter("a", "United States of America"));
+
+
+// /*
+
+// */
